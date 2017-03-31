f6eaffacefacdf3cf0d1e8433e97556476487ced
Donna McMaster <donna@donnamcmaster.com>
2017-03-30 22:03:13 -0700

    add braces around object reference

--- lib/body-class.php
+++ lib/body-class.php
@@ -9,7 +9,7 @@ add_filter( 'body_class', function( $classes ) {
     global $post;
 
     if ( isset( $post ) ) {
-    	$classes[]= "$post->post_type-{$post->post_name}";
+    	$classes[]= "{$post->post_type}-{$post->post_name}";
     }
     return $classes;
 });
