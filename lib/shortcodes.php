a4ad63761b703f1b55838cab822500acb87b8b6d
Donna McMaster <donna@donnamcmaster.com>
2017-04-17 19:23:05 -0700

    added mcw-button shortcode
    - also fixed minor indent inconsistency in existing shortcodes

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -7,22 +7,43 @@
 
 add_shortcode( 'author', function( $atts, $content=null ) {
     ob_start();
-    ?>
-    <div class='author'>
+?>
+    <div class="author">
         <span class='leader'>–</span> <span class='name'><?= empty( $atts['name'] ) ? "" : esc_html( $atts['name'] ); ?></span>
     </div>
-    <?php
 
+<?php
     return ob_get_clean();
 });
 
 add_shortcode( 'highlight', function( $atts, $content=null ) {
     ob_start();
-    ?>
-    <div class='highlight'>
+?>
+    <div class="highlight">
         <?= $content; ?>
     </div>
-    <?php
 
+<?php
+    return ob_get_clean();
+});
+
+add_shortcode( 'mcw-button', function( $atts, $content=null ) {
+	extract( shortcode_atts(
+		array(
+			'title' => '',
+			'link'	=> '',
+			'class' => 'btn btn-primary',
+		), 
+		$atts 
+	));
+	if ( !$title || !$url ) {
+		mcw_log( "mcw_button: title is $title; link is $link" );
+	}
+
+    ob_start();
+?>
+    <a class="<?= $class; ?>" url="<?= $link; ?>"><?= $title; ?></a>
+
+<?php
     return ob_get_clean();
 });
