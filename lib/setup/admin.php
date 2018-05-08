ccdfdd606c12725a9f11420096bc7b4e1eba2dac
Donna McMaster <donna@donnamcmaster.com>
2018-05-07 23:08:02 -0700

    move admin edit lock code to plugin
    it's now an add-on to McPik Basics

--- lib/setup/admin.php
+++ lib/setup/admin.php
@@ -6,9 +6,6 @@
  * @package McBoots
  */
 
-// soften post lock to post warning
-add_filter( 'show_post_locked_dialog', '__return_false' );
-
 add_filter( 'admin_init', function () {
 	// block access to /wp-admin if current user can't edit 
 	// (also check Ajax: http://pento.net/2011/06/19/preventing-users-from-accessing-wp-admin/)
